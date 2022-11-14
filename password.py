8e1b7cf61cc2926a2e78eed67c9c01b4f8ac2164
Valeriy Kozyar <51660599+KozyarValeriy@users.noreply.github.com>
2022-11-14 10:15:21 +0300

    Update password.py
    fixed password generator

--- password.py
+++ password.py
@@ -1,19 +1,16 @@
 import random
+import string
 
 
-def password_generator(count=8):
-    arr = ['q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y', 'u', 'U',
-           'i', 'I', 'o', 'O', 'p', 'P', 'a', 'A', 's', 'S', 'd', 'D', 'f', 'F',
-           'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K', 'l', 'L', 'z', 'Z', 'x', 'X',
-           'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M',
-           '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-           '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+',
-           '?', ',', '.', '[', ']', '{', '}']
-    print("".join(random.sample(arr, count)))
+def password_generator(count: int = 8, with_punctuations: bool = False) -> str:
+    letters = string.ascii_letters + string.digits
+    if with_punctuations:
+        letters += string.punctuation
+    return "".join(random.sample(letters, count))
 
 
 if __name__ == '__main__':
     print('--- START ---')
     while True:
-        n = int(input('Введике кол-во символов в пароле: '))
-        password_generator(n)
+        n = int(input('Введите кол-во символов в пароле: '))
+        print(password_generator(n))
